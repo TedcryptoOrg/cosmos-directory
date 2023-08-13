9c89697c5f1d1222dc5e4b70ccfe9d9714b45d93
Jose Lopes <josluis.lopes@gmail.com>
2023-08-13 13:58:11 +0100

    chore: improve exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,42 @@
 import CosmosDirectory from "./CosmosDirectory";
 import ChainDirectory from "./ChainDirectory";
 import ValidatorsDirectory from "./ValidatorsDirectory";
+import {type Asset} from "./types/ChainDirectory/Asset";
+import {type AssetsResponse} from "./types/ChainDirectory/AssetsResponse";
+import {type Chain} from "./types/ChainDirectory/Chain";
+import {type ChainResponse} from "./types/ChainDirectory/ChainResponse";
+import {type Explorer} from "./types/ChainDirectory/Explorer";
+import {type Fee} from "./types/ChainDirectory/Fee";
+import {type Params} from "./types/ChainDirectory/Params";
+import {type StakingToken} from "./types/ChainDirectory/StakingToken";
+import {type ChainValidatorResponse} from "./types/ValidatorDirectory/ChainValidatorResponse";
+import {type Restake} from "./types/ValidatorDirectory/Restake";
+import {type Validator} from "./types/ValidatorDirectory/Validator";
+import {type ValidatorChain} from "./types/ValidatorDirectory/ValidatorChain";
+import {type ValidatorResponse} from "./types/ValidatorDirectory/ValidatorResponse";
+import {type ValidatorSlash} from "./types/ValidatorDirectory/ValidatorSlash";
+import {type ValidatorsResponse} from "./types/ValidatorDirectory/ValidatorsResponse";
 
-export { CosmosDirectory, ChainDirectory, ValidatorsDirectory };
+export {
+    CosmosDirectory,
+    ChainDirectory,
+    ValidatorsDirectory,
+
+    // types
+    Asset,
+    AssetsResponse,
+    Chain,
+    ChainResponse,
+    Explorer,
+    Fee,
+    Params,
+    StakingToken,
+
+    ChainValidatorResponse,
+    Restake,
+    Validator,
+    ValidatorChain,
+    ValidatorResponse,
+    ValidatorSlash,
+    ValidatorsResponse,
+};
